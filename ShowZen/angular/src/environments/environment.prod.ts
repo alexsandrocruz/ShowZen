4ad58028854e5b25dada5cfbe756d30e4ac222c6
AlexSandro Cruz <alexcruzti@gmail.com>
2026-03-16 14:46:33 -0300

    fix: Normalize redirect URIs by removing trailing slashes for production OIDC compliance

--- ShowZen/angular/src/environments/environment.prod.ts
+++ ShowZen/angular/src/environments/environment.prod.ts
@@ -4,13 +4,13 @@ import { Environment } from '@abp/ng.core';
 export const environment = {
   production: true,
   application: {
-    baseUrl: 'https://eventos.zensuite.com.br/',
+    baseUrl: 'https://eventos.zensuite.com.br',
     name: 'ShowZen',
     logoUrl: '',
   },
   oAuthConfig: {
     issuer: 'https://eventos.zensuite.com.br/',
-    redirectUri: 'https://eventos.zensuite.com.br/',
+    redirectUri: 'https://eventos.zensuite.com.br',
     clientId: 'ShowZen_App',
     responseType: 'code',
     scope: 'offline_access openid profile email phone ShowZen',
